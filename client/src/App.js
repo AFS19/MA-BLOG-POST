988b1ee199a4c4fa76086b29ba0f9561962f567e
AFS19 <iireco20@gmail.com>
2023-12-23 18:26:45 +0000

    create all pages I need for this sample Blog project

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,46 @@
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Write from "./pages/Write";
+import Single from "./pages/Single";
+import Main from "./Layouts/Main";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello, World</h1>
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
