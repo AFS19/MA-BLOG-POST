811ff1de93d5b40616f9d896055a740f0520844a
AFS19 <iireco20@gmail.com>
2023-12-31 20:49:14 +0000

    create footer section with basic style and install react icons to use it

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,3 +1,25 @@
+import { Link } from "react-router-dom";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="container">
+        <img src="./img/mablog.png" alt="" />
+        <div className="socials">
+          <Link to="#mablog.instagram.com" className="link">
+            <FaInstagram className="socials-icon" />
+          </Link>
+          <Link to="#mablog.twitter.com" className="link">
+            <FaFacebook className="socials-icon" />
+          </Link>
+          <Link to="#mablog.linkedin.com" className="link">
+            <FaLinkedin className="socials-icon" />
+          </Link>
+        </div>
+
+        <div className="copyright">mablog © 2023 • Publié avec m.afs</div>
+      </div>
+    </footer>
+  );
 }
