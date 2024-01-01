c4696a6d50c7f78557d0db3e256c9831b38ed484
AFS19 <iireco20@gmail.com>
2024-01-01 20:35:51 +0000

    start working on single page and the style also

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,3 +1,17 @@
 export default function Single() {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <div className="content">
+        <img src="#" alt="" />
+        <div className="user">
+          <img src="#user" alt="" />
+          <div className="info">
+            <span>Mohamed Afssas</span>
+            <p>Posted 2 days ago</p>
+          </div>
+        </div>
+      </div>
+      <div className="menu">Menu</div>
+    </div>
+  );
 }
