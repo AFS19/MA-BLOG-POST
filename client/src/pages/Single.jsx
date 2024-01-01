d2b3f3c7da572fe8f5389013856e490d1b0ce160
AFS19 <iireco20@gmail.com>
2024-01-01 21:49:03 +0000

    complete code of single page

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,6 +1,10 @@
+import { Link } from "react-router-dom";
+// import Menu from "../components/Menu";
+
 export default function Single() {
   return (
     <div className="single">
+      {/* content */}
       <div className="content">
         <img src="#" alt="" />
         <div className="user">
@@ -9,9 +13,67 @@ export default function Single() {
             <span>Mohamed Afssas</span>
             <p>Posted 2 days ago</p>
           </div>
+          <div className="actions">
+            <Link to={`/write?edit=2`}>
+              <img
+                src="/img/edit.png"
+                alt="edit post"
+                title="edit"
+                className="edit"
+              />
+            </Link>
+            <img
+              src="/img/delete.png"
+              alt="delete post"
+              title="delete"
+              className="delete"
+            />
+          </div>
         </div>
+        <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
+        <p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            similique nemo autem odit reiciendis eveniet, porro quis explicabo
+            alias dolor. Consequatur unde laboriosam commodi, facere architecto
+            illo totam porro. Architecto.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            similique nemo autem odit reiciendis eveniet, porro quis explicabo
+            alias dolor. Consequatur unde laboriosam commodi, facere architecto
+            illo totam porro. Architecto.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            similique nemo autem odit reiciendis eveniet, porro quis explicabo
+            alias dolor. Consequatur unde laboriosam commodi, facere architecto
+            illo totam porro. Architecto.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            similique nemo autem odit reiciendis eveniet, porro quis explicabo
+            alias dolor. Consequatur unde laboriosam commodi, facere architecto
+            illo totam porro. Architecto.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            similique nemo autem odit reiciendis eveniet, porro quis explicabo
+            alias dolor. Consequatur unde laboriosam commodi, facere architecto
+            illo totam porro. Architecto. Lorem ipsum dolor, sit amet
+            consectetur adipisicing elit. Aliquam cumque nulla perferendis fugit
+            perspiciatis assumenda, incidunt veniam odio distinctio nobis
+            eligendi optio expedita labore commodi a ullam amet soluta ipsam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id dolorem
+            distinctio, exercitationem laudantium impedit quae voluptate, sit
+            iste dolorum eius saepe. Minus eius natus ut nulla esse porro
+            doloremque magni!
+          </p>
+        </p>
       </div>
-      <div className="menu">Menu</div>
+
+      {/* Menu */}
+      {/* <Menu /> */}
     </div>
   );
 }
