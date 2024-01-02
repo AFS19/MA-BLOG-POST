8bb69a8c5345521343604dc507ae4a54b815a862
AFS19 <iireco20@gmail.com>
2024-01-02 20:19:59 +0000

    start working on the write page

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,3 +1,11 @@
 export default function Write() {
-  return <div>Write</div>;
+  return (
+    <div className="write-container">
+      <div className="content">content</div>
+      <div className="menu">
+        <div className="item">Item 1</div>
+        <div className="item">Item 2</div>
+      </div>
+    </div>
+  );
 }
