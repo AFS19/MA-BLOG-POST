1aa95028300441478de7f0cb911f24dcb157eb99
AFS19 <iireco20@gmail.com>
2024-01-22 21:33:11 +0000

    initial routes folder for my app

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import postRouter from "./routes/Posts.js";
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
+app.use("api/posts", postRouter);
 
 app.listen(PORT, console.log(`server run on port: ${PORT}`));
