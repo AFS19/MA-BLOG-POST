6841c29ef8b2fcc1c9ad8329e0fe42a4956eae3e
AFS19 <iireco20@gmail.com>
2024-01-25 22:16:03 +0000

    start working on register method on the auth controller

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -1,9 +1,15 @@
+import db from "../db/db.js";
+
 export const register = (req, res) => {
-  res.json([
-    (data) => {
-      "register method from auth controller";
-    },
-  ]);
+  // collect data
+  const [username, email, password] = req.body;
+  // check existing user
+  const query = `SELECT username, email FROM users WHERE username=? AND email=?`;
+  db.query(query, [username, email], (error, data) => {
+    if (error) return res.json([(data) => error]);
+    if (data.length)
+      return res.status(409).json([(data) => "this user already exists."]);
+  });
 };
 export const login = (req, res) => {
   res.json([
