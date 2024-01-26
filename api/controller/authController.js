43880ed34e891492f37af5bcc70b8720eca29a5a
AFS19 <iireco20@gmail.com>
2024-01-26 23:33:04 +0000

    hash password and create new user functionality

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -1,4 +1,5 @@
 import db from "../db/db.js";
+import bcrypt from "bcryptjs";
 
 export const register = (req, res) => {
   // collect data
@@ -9,6 +10,16 @@ export const register = (req, res) => {
     if (error) return res.json([(data) => error]);
     if (data.length)
       return res.status(409).json([(data) => "this user already exists."]);
+    // Hash the password
+    const  slat = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.has(password, salt);
+    // Create new user
+    const query = "INSERT INTO users(`username`,`email`,`password`) VALUES (?, ?, ?)";
+    const values = [username, email, hashPassword];
+    db.query(query, values, (error) => {
+      if (error) return res.json([(data) => error]);
+      return res.status(200).json("user create success");
+    });
   });
 };
 export const login = (req, res) => {
