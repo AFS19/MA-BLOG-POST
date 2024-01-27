f6e683c2de7f2ff58c829bf4e75c4a86dec9d0e4
AFS19 <iireco20@gmail.com>
2024-01-27 21:49:28 +0000

    start implement the functionality on the register UI

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Register() {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: ""
+  });
+  const handleChange = (e) => {
+    setInputs((previous) => ({
+      ...previous,
+      [e.target.id]: e.target.value,
+    }));
+  }
   return (
     <div className="auth">
       <h1>Register</h1>
-      <form action="">
-        <input type="text" required id="username" placeholder="username" />
-        <input type="email" required id="email" placeholder="email" />
-        <input type="password" required id="password" placeholder="password" />
+      <form onSubmit={handleChange}>
+        <input type="text" required id="username" name="username" placeholder="username" />
+        <input type="email" required id="email" name="email" placeholder="email" />
+        <input type="password" required id="password" name="password" placeholder="password" />
         <button>Register</button>
         <p className="error">This is an error!</p>
         <span>
